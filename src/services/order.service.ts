a0883b60a98ef23d0936b5f3ee8e6de953c9f4a1
AlexsandroVS <alexsandro.valeriano1@gmail.com>
2025-07-03 15:15:24 -0500

    control2

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -105,7 +105,7 @@ export const OrderService = {
           create: {
             gateway: 'manual',
             amount: total,
-            status: 'succeed',
+            status: 'succeeded',
             metadata: {}
           }
         }
